a05aac4aa15e97b4becca26ae38cbb95b6819ca9
XdotCore <xandercorvalan@gmail.com>
2024-03-29 20:56:15 -0500

    Revert "Merge branch 'follow_me' of https://github.com/ghoulslash/pokeemerald into pokearceus"
    This reverts commit a8cdcd45ab60f5d07bd94fb75cafc87da3be97a6.

--- src/party_menu.c
+++ src/party_menu.c
@@ -63,7 +63,6 @@
 #include "trade.h"
 #include "union_room.h"
 #include "window.h"
-#include "follow_me.h"
 #include "constants/battle.h"
 #include "constants/battle_frontier.h"
 #include "constants/field_effects.h"
@@ -3829,9 +3828,6 @@ static void FieldCallback_Surf(void)
 
 static bool8 SetUpFieldMove_Surf(void)
 {
-    if (!CheckFollowerFlag(FOLLOWER_FLAG_CAN_SURF))
-        return FALSE;
-    
     if (PartyHasMonWithSurf() == TRUE && IsPlayerFacingSurfableFishableWater() == TRUE)
     {
         gFieldCallback2 = FieldCallback_PrepareFadeInFromMenu;
@@ -3851,9 +3847,6 @@ static void DisplayCantUseSurfMessage(void)
 
 static bool8 SetUpFieldMove_Fly(void)
 {
-    if (!CheckFollowerFlag(FOLLOWER_FLAG_CAN_LEAVE_ROUTE))
-        return FALSE;
-    
     if (Overworld_MapTypeAllowsTeleportAndFly(gMapHeader.mapType) == TRUE)
         return TRUE;
     else
@@ -3874,9 +3867,6 @@ static void FieldCallback_Waterfall(void)
 static bool8 SetUpFieldMove_Waterfall(void)
 {
     s16 x, y;
-    
-    if (!CheckFollowerFlag(FOLLOWER_FLAG_CAN_WATERFALL))
-        return FALSE;
 
     GetXYCoordsOneStepInFrontOfPlayer(&x, &y);
     if (MetatileBehavior_IsWaterfall(MapGridGetMetatileBehaviorAt(x, y)) == TRUE && IsPlayerSurfingNorth() == TRUE)
@@ -3896,9 +3886,6 @@ static void FieldCallback_Dive(void)
 
 static bool8 SetUpFieldMove_Dive(void)
 {
-    if (!CheckFollowerFlag(FOLLOWER_FLAG_CAN_DIVE))
-        return FALSE;
-    
     gFieldEffectArguments[1] = TrySetDiveWarp();
     if (gFieldEffectArguments[1] != 0)
     {
